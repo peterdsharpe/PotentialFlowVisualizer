569cc5b3a10ea94fb07290fd69bd54063686b221
Peter Sharpe <pds@mit.edu>
2023-09-14 13:44:15 -0400

    update draw routines

--- potentialflowvisualizer/flowfield.py
+++ potentialflowvisualizer/flowfield.py
@@ -26,12 +26,16 @@ class Flowfield:
         return sum([object.get_y_velocity_at(points) for object in self.objects])
 
     def draw(self,
-             scalar_to_plot:str="potential",  # "potential", "streamfunction", "xvel", "yvel", "velmag", "Cp"
-             x_points: np.ndarray=np.linspace(-10, 10, 400),
-             y_points: np.ndarray=np.linspace(-10, 10, 300),
-             percentiles_to_include = 99.7,
-             show=True,
+             scalar_to_plot: str = "potential",  # "potential", "streamfunction", "xvel", "yvel", "velmag", "Cp"
+             x_points: np.ndarray = np.linspace(-10, 10, 400),
+             y_points: np.ndarray = np.linspace(-10, 10, 300),
+             percentiles_to_include: float = 99.7,
+             set_equal: bool = True,
+             show: bool = True,
              ):
+        import matplotlib.pyplot as plt
+        import aerosandbox.tools.pretty_plots as p
+
         X, Y = np.meshgrid(x_points, y_points)
         X_r = np.reshape(X, -1)
         Y_r = np.reshape(Y, -1)
@@ -44,7 +48,7 @@ class Flowfield:
         elif scalar_to_plot == "xvel":
             scalar_to_plot_value = sum([object.get_x_velocity_at(points) for object in self.objects])
         elif scalar_to_plot == "yvel":
-            scalar_to_plot_value= sum([object.get_y_velocity_at(points) for object in self.objects])
+            scalar_to_plot_value = sum([object.get_y_velocity_at(points) for object in self.objects])
         elif scalar_to_plot == "velmag":
             x_vels = sum([object.get_x_velocity_at(points) for object in self.objects])
             y_vels = sum([object.get_y_velocity_at(points) for object in self.objects])
@@ -60,20 +64,77 @@ class Flowfield:
         min = np.nanpercentile(scalar_to_plot_value, 50 - percentiles_to_include / 2)
         max = np.nanpercentile(scalar_to_plot_value, 50 + percentiles_to_include / 2)
 
-        contour(
+        p.contour(
             x_points, y_points, scalar_to_plot_value.reshape(X.shape),
             levels=np.linspace(min, max, 80),
             linelabels=False,
             cmap=plt.get_cmap("rainbow"),
             contour_kwargs={
                 "linestyles": 'solid',
-                "alpha": 0.4
+                "alpha"     : 0.4
             }
         )
-        plt.gca().set_aspect("equal", adjustable='box')
-        show_plot(
+
+        if set_equal:
+            p.equal()
+
+        p.show_plot(
             f"Potential Flow: {scalar_to_plot}",
             "$x$",
             "$y$",
+            show=show
+        )
+
+    def draw_streamlines(self,
+                         x_points: np.ndarray = np.linspace(-10, 10, 400),
+                         y_points: np.ndarray = np.linspace(-10, 10, 300),
+                         cmap=None,
+                         norm=None,
+                         set_equal: bool = True,
+                         show: bool = True,
+                         ):
+        if cmap is None:
+            cmap = "turbo"
+
+        X, Y = np.meshgrid(x_points, y_points)
+        U = np.reshape(
+            self.get_x_velocity_at(np.stack([
+                X.flatten(),
+                Y.flatten()
+            ], axis=1)),
+            X.shape
+        )
+        V = np.reshape(
+            self.get_y_velocity_at(np.stack([
+                X.flatten(),
+                Y.flatten()
+            ], axis=1)),
+            X.shape
+        )
+
+        velmag = np.sqrt(U ** 2 + V ** 2)
+
+        import matplotlib.pyplot as plt
+        import aerosandbox.tools.pretty_plots as p
+
+        plt.streamplot(
+            X, Y, U, V,
+            color=velmag,
+            linewidth=1,
+            minlength=0.02,
+            cmap=cmap,
+            norm=norm,
+            broken_streamlines=False
+        )
+
+        plt.colorbar(label="Velocity Magnitude [m/s]")
+
+        if set_equal:
+            p.equal()
+
+        p.show_plot(
+            "Potential Flow Simulation of Room Flow",
+            r"$x$ [m]",
+            r"$y$ [m]",
             show=show
         )
