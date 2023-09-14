4651c68446a3cbb2aed633e241a6aea92831e101
Peter Sharpe <pds@mit.edu>
2023-09-14 17:11:55 -0400

    add streamline plot

--- examples/lifting_cylinder_flow.py
+++ examples/lifting_cylinder_flow.py
@@ -3,8 +3,11 @@ from potentialflowvisualizer import *
 field = Flowfield(
     objects=[
         Freestream(1, 0),
-        Vortex(20, 0, 0),
-        Doublet(-100, 0, 0, 0)
+        # Source(1, 0, 0),
+        Vortex(-20, 0, 0),
+        Doublet(-100, 0, 0, 0),
+        # LineSource(1, 0, 0, 0, 5),
+        # LineVortex(1, 0, 0, 0, 5),
     ]
 )
 
@@ -13,3 +16,4 @@ field.draw("streamfunction")
 # field.draw("xvel")
 # field.draw("yvel")
 # field.draw("velmag")
+field.draw_streamlines()
